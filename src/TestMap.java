049e927437ee035178510e2c95c3cc9a985251a2
Md Shamsheer Ala <shamshir.alam@gmail.com>
2018-09-06 12:24:40 +0530

    New File

---
+++ src/TestMap.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMap {
+
+	public static void main(String[] args) {
+		final Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("A",1);
+		map.put("B",2);
+		map.put("C",3);
+		map.put("D",4);
+		System.out.println(map);
+		//map = new HashMap<>();
+		
+
+	}
+
+}
